fd0b9baf39ff684d883db67e1a2e6345170077bc
Andision <48558940+Andision@users.noreply.github.com>
2022-09-30 18:21:09 +0800

    Init

---
+++ resource/plugin/i18n/jp_zh_CN.js
@@ -0,0 +1,378 @@
+// ============================以下为tp_up 国际化翻译定义  开始==========================
+//allnotice_*  全部通知  开始
+var allnotice_successful_collection="收藏成功!";
+var allnotice_cancel_collection_success="取消收藏成功!";
+var allnotice_contact_the_administrator="未查询到相关内容，请联系管理员！";
+var allnotice_no_related_information_found="未查询到相关信息！";
+//allnotice_*  全部通知  结束
+
+//notes_*  便签组件  开始
+var notes_saved_by="保存于 ";
+var notes_word_entered="已输入字数：";
+//notes_*  便签组件  结束
+
+//myinfoim_*  我的信息组件  开始
+var myinfoim_normal="正常";
+var myinfoim_current_user="当前用户不存在于图书馆系统中。";
+var myinfoim_fetching_error="获取异常，请刷新页面。";
+var myinfoim_not_in_my_cards_system="当前用户不存在于一卡通系统中。";
+var myinfoim_account_is_frozen="账户已挂失或者已销户。";
+var myinfoim_not_in_self_service_system="当前用户不存在于自助系统中。";
+var myinfoim_not_bind_an_email_address="您还未绑定邮箱。";
+//myinfoim_*  我的信息组件  结束
+
+//applications_*  常用应用组件   开始
+var applications_hide_all_applications="收起全部应用";
+var applications_expand_all_applications="展开全部应用";
+var applications_info_entered_do_not_match="输入号与个人信息不一致。";
+var applications_enter_phone_number="请输入正确的手机号。";
+var applications_wrong_password="密码输入错误。";
+var applications_retry_after_second="120秒后重新获取。";
+var applications_seconds_to_get=" 秒后重新获取。";
+var applications_passwords_entered_are_different="两次密码输入不同！";
+//applications_*  常用应用组件  结束
+
+//class_*  课程表组件  开始
+var class_this_week="放假中，查询课程请点击此处。";
+var class_please_select="请选择";
+var class_academic_year="学年 第";
+var class_semesters="学期 校历第";
+var class_week="周";
+var class_month="月";
+var class_day="日";
+var class_select_academic_year="请选择学年度";
+var class_select_semester="请选择学期";
+var class_select_week="请选择教学周";
+//class_*  课程表组件  结束
+
+//calendar_mid_*  日历组件  开始
+var calendar_mid_ymd="yyyy年MM月dd日";
+var calendar_mid_ym="yyyy年MM月";
+//calendar_mid_*  日历组件  结束
+
+
+//infocenter_*  信息中心  开始
+var infocenter_no_info="暂时没有新闻信息";
+var infocenter_subscription="订阅";
+var infocenter_subscription_success="订阅成功！";
+var infocenter_all_selection="全部选择";
+//infocenter_*  信息中心  结束
+
+
+//appcenter_*  应用中心  开始
+var appcenter_cancel_collection="取消收藏";
+var appcenter_collection="收藏";
+var appcenter_input_number="输入号与个人信息不一致";
+var appcenter_phone_number="请输入正确的手机号";
+var appcenter_personal_information="输入号与个人信息不一致";
+var appcenter_verification_code="密码或验证码错误";
+var appcenter_set_up_successfully="设置成功！";
+var appcenter_incorrect_password="密码输入错误";
+var appcenter_retry_after_second="120秒后重新获取。";
+var appcenter_seconds_to_get=" 秒后重新获取。";
+var appcenter_password_length="密码长度大于6位";
+var appcenter_passwords_entered_are_different="两次密码输入不同！";
+var appcenter_group_name="请填写分组名称！";
+var appcenter_length_cannot="长度不能大于10个字！";
+var appcenter_common_links="是否删除分组及其关联的常用链接？";
+var appcenter_successfully_deleted="删除成功！";
+var appcenter_selected_record="确认是否删除所选记录？";
+var appcenter_select_group="请选择分组";
+//appcenter_*  应用中心  结束
+
+//feedback_*  问题反馈  开始
+var feedback_all="全部";
+var feedback_response="请输入回复内容";
+var feedback_success="回复成功！";
+var feedback_fail ="回复失败！";
+var feedback_confim="确认后会覆盖原回复内容，是否确认？";
+var feedback_attention="感谢您的关注，欢迎提出更多宝贵意见！";
+var feedback_add_failed="添加失败！";
+var feedback_title="请输入问题标题";
+var feedback_type="请选择问题类型";
+var feedback_upload="上传失败";
+var feedback_reject="已拒绝";
+var feedback_adopted="已采用";
+//feedback_*  问题反馈  结束
+
+
+//calendar_*  日程  开始
+var calendar_yyyymmmm="YYYY[年]MMMM";
+var calendar_year="年";
+var calendar_month="月";
+var calendar_day="日";
+var calendar_jan="1月";
+var calendar_feb="2月";
+var calendar_mar="3月";
+var calendar_apr="4月";
+var calendar_may="5月";
+var calendar_jun="6月";
+var calendar_jul="7月";
+var calendar_aug="8月";
+var calendar_sep="9月";
+var calendar_oct="10月";
+var calendar_nov="11月";
+var calendar_dec="12月";
+var calendar_sun="日";
+var calendar_mon="一";
+var calendar_tue="二";
+var calendar_wed="三";
+var calendar_thu="四";
+var calendar_fri="五";
+var calendar_sat="六";
+var calendar_no_schedule="暂无日程";
+var calendar_drag_and_drop="拖拽失败。";
+var calendar_update_failed="更新失败。";
+var calendar_export="导出";
+var calendar_no_data="无数据！";
+var calendar_no="暂无";
+var calendar_to=" 至 ";
+var calendar_location="地点：";
+var calendar_description="说明：";
+var calendar_publisher=" 发布人：";
+var calendar_please_choose="-请选择-";
+var calendar_personal_calendar="个人日历";
+var calendar_creating="创建个人日历失败。";
+var calendar_shared_staff="共享人员：";
+var calendar_share_staff="分享人员";
+var calendar_editable="可编辑：";
+var calendar_editable_person="可编辑人员";
+var calendar_editable_calendar_staff="可编辑本日历人员";
+var calendar_delete="删除";
+var calendar_add="添加";
+var calendar_share="分享";
+var calendar_edit="编辑";
+var calendar_member_success="成员成功";
+var calendar_member_failure="成员失败";
+var calendar_are_you_sure="确认是否删除该日历";
+var calendar_person="人员？";
+var calendar_select="选取";
+var calendar_calendar_add="日历添加成功！";
+var calendar_the_calendar_was="日历修改成功！";
+var calendar_subscription_calendars="个订阅日历";
+var calendar_confirm_whether_to_delete="确认是否删除日历【";
+var calendar_the_calendar="】？";
+var calendar_confirm_whether_to_cancel="确认是否取消订阅？";
+var calendar_portal_platform="门户平台";
+var calendar_calendar="日程";
+var calendar_share_staffs="分享人员【";
+var calendar_share_staffy="】";
+var calendar_monday="星期一";
+var calendar_tuesday="星期二";
+var calendar_wednesday="星期三";
+var calendar_thursday="星期四";
+var calendar_friday="星期五";
+var calendar_saturday="星期六";
+var calendar_sunday="星期天";
+var calendar_every_day="每天";
+var calendar_every="每隔 ";
+var calendar_days=" 天";
+var calendar_times="次";
+var calendar_until="直到 ";
+var calendar_weekly="每周  ";
+var calendar_week=" 周    " ;
+var calendar_and="和";
+var calendar_months="月";
+var calendar_per_month="每月";
+var calendar_years="年";
+var calendar_per_year="每年 ";
+var calendar_in=" 在  ";
+var calendar_on_the="在第    ";
+var calendar_in_the="在第    ";
+var calendar_in_the_week="个星期  ";
+var calendar_view_all="查看全部";
+var calendar_unsubscribe="取消订阅";
+var calendar_subscribe="订阅";
+var calendar_an_administrator="您已经是管理员，不要在重复添加该日历!";
+var calendar_subscribed="已订阅";
+var calendar_not_subscribed="未订阅";
+var calendar_successful_operation="操作成功";
+var calendar_operation_failed="操作失败";
+//calendar_*  日程  结束
+// ===============================以上为tp_up 国际化翻译定义  结束============================================
+
+
+// ===================================以下为tp_core 国际化翻译定义  开始========================================
+//msgtake_*  消息订阅公共弹出接口  开始
+var msgtake_reminder_time="请选择提醒时间";
+var msgtake_added_successfully="添加成功！";
+var msgtake_reminder_channel="请选择提醒通道";
+var msgtake_reminder_cycle="请选择提醒周期";
+//msgtake_*  消息订阅公共弹出接口  结束
+
+//mymessages_*  我的消息   开始
+var mymessages_successfully_saved="保存成功!";
+var mymessages_save_failed="保存失败!";
+var mymessages_try_again="请重试!";
+var mymessages_have_been_read="确认全部已读？";
+var mymessages_fail_to_edit="修改失败!";
+var mymessages_confirm_deletion="确认删除？";
+var mymessages_confirm_all_deleted="确认全部删除？";
+var mymessages_select_reminder_time="请选择提醒时间。";
+var mymessages_successfully_modified="修改成功！";
+var mymessages_unread="未读！";
+var mymessages_mark_as_read="标为已读";
+var mymessages_delete_message="删除消息";
+var mymessages_sms_message="短信信息";
+var mymessages_wechat_information="微信信息";
+var mymessages_enable="启用";
+var mymessages_shut_down="关闭";
+var mymessages_edit="编辑";
+var mymessages_deletes="删除";
+var mymessages_sms="短信";
+var mymessages_wechat="微信";
+var mymessages_mails="邮件";
+var mymessages_portal_message="门户消息";
+var mymessages_mon="周一";
+var mymessages_tue="周二";
+var mymessages_wed="周三";
+var mymessages_thur="周四";
+var mymessages_fri="周五";
+var mymessages_sat="周六";
+var mymessages_sun="周日";
+var mymessages_select_a_reminder_channel="请选择提醒通道";
+var mymessages_select_a_reminder_cycle="请选择提醒周期";
+var mymessages_count_unread="未读";
+var mymessages_count_read="已读";
+//mymessages_*  我的消息   结束
+
+//myinfo_*  我的信息  开始
+var myinfo_successfully_modified="修改成功！";
+var myinfo_teaching_contact="教学联系人";
+var myinfo_institutional_contact="机构联系人";
+var myinfo_online="在线";
+var myinfo_illegal_operation="非法操作！";
+var myinfo_default_avatar_is_not_maintained="抱歉，暂未维护默认头像！";
+var myinfo_delete_this_avatar="确认删除该头像吗？";
+var myinfo_set_up_successfully="设置成功！";
+var myinfo_login_alias_already_exists="登录别名已存在";
+var myinfo_the_micro_signal="微信号仅支持6-20个字母、数字、下划线或减号，并以字母开头";
+var myinfo_QQ_account_format_error="QQ账号格式错误";
+var myinfo_inconsistent_passwords="前后密码不一致";
+var myinfo_compression_failed="压缩失败！";
+var myinfo_select_an_image="请选择10M以内的图片上传！";
+var myinfo_upload_failed="上传失败！";
+var myinfo_please_select="请选择";
+var myinfo_account_number="账号";
+var myinfo_namess="姓名";
+var myinfo_gender="性别";
+var myinfo_identity_types="身份类型";
+var myinfo_affiliation="所属机构";
+var myinfo_user_category="用户类型";
+var myinfo_user_status="用户状态";
+var myinfo_position_title="岗位名称";
+var myinfo_not_available="暂无";
+var myinfo_office_phones="办公电话";
+var myinfo_e_mail="邮箱";
+var myinfo_classs="班级";
+var myinfo_statuss="状态";
+var myinfo_mobile_phone="手机";
+var myinfo_tutor="导师";
+var myinfo_the_mailbox_name="邮箱名称只可包含 字母 数字 - _ .的字符，且长度在3-19之间";
+var myinfo_set_a_password_with_a_length="请设置一个长度为8-24的密码，同时建议定期更换密码。";
+var myinfo_a_highly_secure_password="安全性高的密码可以使账号更安全，请设置一个包含字母、符号、数字中至少两种且长度超过8位的密码，同时建议定期更换密码。";
+var myinfo_a_highly_secure_passwords="安全性高的密码可以使账号更安全，请设置一个包含字母、符号、数字且长度为8-24位的密码，同时建议定期更换密码。";
+var myinfo_a_highly_secure_passwordss="安全性高的密码可以使账号更安全，请设置一个包含字母、符号、数字中至少两种且长度超过8位的密码，同时建议定期更换密码。";
+var myinfo_inconsistent_password="两次密码输入不一致";
+var myinfo_weak="弱";
+var myinfo_moderate="中";
+var myinfo_strongs="强";
+var myinfo_new_password_does_not_match="新密码与确认密码不一致。";
+var myinfo_user_does_not_exist="用户不存在。";
+var myinfo_original_password_is_incorrect="原始密码错误。";
+var myinfo_failed_to_change_password="修改密码失败。";
+var myinfo_tpassword_does_not_match_the_rules="密码不符合规则。";
+var myinfo_the_original_mobile_number="请填入与原手机号不同的号码！";
+var myinfo_secret_security_setting="密保设置手机验证码";
+var myinfo_secret_mobile_phone="修改密保手机";
+var myinfo_re_acquire="获取短信验证码失败，请重新获取！";
+var myinfo_enter_phone_number="请输入正确的手机号";
+var myinfo_register_the_school_mailbox="系统没有检测到与您相关的校内邮箱，请在此页面注册校内邮箱。";
+var myinfo_an_on_campus_email ="如果您已有校内邮箱，请点击";
+var myinfo_heres ="这里";
+var myinfo_to_bind ="进行绑定。";
+var myinfo_the_school_mailbox ="系统没有检测到与您相关的校内邮箱，如果已有校内邮箱，请您在此页面进行绑定";
+var myinfo_an_on_campus_email_address ="如果您还未拥有校内邮箱，请点击 ";
+var myinfo_registereds ="注册";
+var myinfo_to_apply ="进行申请。";
+var myinfo_the_school_mailbox_associated ="系统已检测到与您相关的校内邮箱";
+var myinfo_in_school_mailbox ="您可以直接选择绑定检测出的校内邮箱";
+var myinfo_by_email_authentication ="也可以通过邮箱验证绑定其他校内邮箱。";
+var myinfo_does_not_meet_the_requirements ="身份证件号不符合要求，";
+var myinfo_please_visit ="请您访问";
+var myinfo_self_service_email ="(仅限校内访问)自助申请邮箱或者手动绑定。";
+var myinfo_detect_the_campus_mailbox_associated ="系统没有检测到与您相关的校内邮箱，请手动绑定。";
+var myinfo_school_mailbox_system_is_busy ="校内邮箱系统忙，请稍后再试。";
+var myinfo_select_the_mailbox_suffix ="请选择需要绑定的邮箱后缀。";
+var myinfo_enter_the_mailbox_you_need_to_bind ="请输入需要绑定的邮箱。";
+var myinfo_setting_mailbox_verification_code ="密保设置邮箱验证码。";
+var myinfo_get_the_email_verification_code ="获取邮件验证码失败，请重新获取！";
+var myinfo_choose_the_school_mailbox_to_bind ="请选择校邮箱进行绑定！";
+var myinfo_email_registration_is_successful ="邮箱注册成功，请牢记您的邮箱账号密码。";
+var myinfo_email_verification_passed ="邮箱验证通过！";
+var myinfo_enter_your_vaild_email ="请输入正确的邮箱！";
+var myinfo_mailbox_system_is_busy ="邮箱系统忙，请稍后再试！";
+var myinfo_modifying_the_secret_mailbox_failed ="修改密保邮箱失败！";
+var myinfo_self_service_account ="自助账号不存在！";
+var myinfo_failed_to_open_billing_account ="开通计费账号失败！";
+var myinfo_email_address_is_already_occupied ="邮箱地址已被占用！";
+var myinfo_calling_the_mail_system ="调用邮件系统创建邮箱失败！";
+var myinfo_mail_service_already_exists ="邮件服务已存在！";
+var myinfo_mail_service_template_package ="邮件服务模板套餐未配置！";
+var myinfo_sam_mail_service_failed ="SAM邮件服务开通失败！";
+var myinfo_email_application_failed ="邮箱申请失败！";
+var myinfo_contain_chinese ="密码不能含有中文！";
+var myinfo_please_enter_a_digit_password ="请输入8-24位的密码！";
+var myinfo_include_three_or_more_identical ="密码中不能同时包含三个及以上相同字母或数字！";
+var myinfo_enter_a_password_of_digits ="请输入8-24位的密码，同时建议定期更换密码！";
+var myinfo_a_highly_secure_passwordssss ="安全性高的密码可以使账号更安全，请设置一个包含字母、符号、数字中至少两种且长度超过8-24位的密码，同时建议定期更换密码。";
+var myinfo_a_highly_secure_passwordsssss ="安全性高的密码可以使账号更安全，请设置一个包含字母、符号、数字且长度超过8-24位的密码，同时建议定期更换密码。";
+var myinfo_regain_after_60_seconds ="60秒后重新获取";
+var myinfo_seconds_to_get=" 秒后重新获取。";
+var myinfo_log_in_time=" 登录时间";
+var myinfo_operatorss=" 操作人";
+var myinfo_operator_department=" 操作人所属部门";
+var myinfo_operating_system=" 操作系统";
+var myinfo_browserss=" 浏览器";
+var myinfo_operation_type="操作类型";
+var myinfo_log_in="登录";
+var myinfo_drop_out="退出";
+var myinfo_username_or_password_is_wrong="用户名密码错误";
+var myinfo_user_status_is_incorrect="用户状态有误";
+var myinfo_code_is_incorrect="验证码有误";
+var myinfo_login_ip_is_disabled="登录IP被禁用";
+var myinfo_login_time_is_forbidden="登录时间被禁止";
+var myinfo_ket_verification_failed="ticket校验失败";
+var myinfo_user_login_exception="用户登录异常";
+var myinfo_login_exception="登录异常";
+var myinfo_request_path="请求路径";
+var myinfo_data_synchronization="数据同步中";
+var myinfo_login_place="登录地";
+//myinfo_*  我的信息  结束
+
+
+//uam_*  选人组件  开始
+var uam_fold="折叠";
+var uam_expand="展开";
+var uam_or="或";
+var uam_same_object="不能重复选择同一对象！";
+var uam_delete="删除";
+var uam_people="人";
+var uam_upload_success="上传成功";
+var uam_partially="部分上传成功";
+var uam_upload_failed="上传失败";
+var uam_type_error="上传失败,文件类型错误";
+var uam_selected_object="已选对象超过最大值,请减少选择对象！";
+var uam_identity_type="身份类型";
+var uam_select="选取";
+var uam_general_position="通用岗位";
+var uam_departmental_position="部门岗位";
+//uam_*  选人组件  结束
+// =================================以上为tp_core 国际化翻译定义  结束==========================================
+
+
+
+
+
+
+
